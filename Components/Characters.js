1caae2e153e575ce738446cca4fd633e00ad1a70
josephenoch <enochjoseph01@gmail.com>
2022-05-14 23:48:17 +0100

    :lipstick: styled the characters section

--- Components/Characters.js
+++ Components/Characters.js
@@ -3,6 +3,11 @@ import Image from "next/image";
 import styles from '../styles/Home.module.css'
 
 const Characters = ({characters}) =>{
+  const textStyle ={
+        textOverflow:"ellipsis", 
+        whiteSpace:"nowrap", 
+        overflow:"hidden"
+    }
   return (<> 
       {characters.map((character)=>{
           return(
@@ -10,12 +15,14 @@ const Characters = ({characters}) =>{
                     href={character.image}
                     className={styles.card}
                     target="_blank"
-                    key={character.id}>
+                    key={character.id}
+                    rel="noreferrer"
+                >
 
-                    <Image src={character.image} width={300} height={300}/>
-                    <h2>{character.name}</h2>
-                    <p>Origin:{character.origin.name}</p>
-                    <p>Location:{character.location.name}</p>
+                    <Image src={character.image} width={300} height={300} alt={character.name}/>
+                    <h2 style={textStyle}>{character.name}</h2>
+                    <p style={textStyle}>Origin:{character.origin.name}</p>
+                    <p style={textStyle}>Location:{character.location.name}</p>
                 </a>
           )
       })}
